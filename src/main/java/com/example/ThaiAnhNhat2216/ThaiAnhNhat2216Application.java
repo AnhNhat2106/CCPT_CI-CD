5d571d82acc34aa9683ef339e5f5728da1513180
ThaiAnhNhat2280602216 <nhatthai.210604@gmail.com>
2026-03-19 10:43:11 +0700

    Them code va GitHub Actions

---
+++ src/main/java/com/example/ThaiAnhNhat2216/ThaiAnhNhat2216Application.java
@@ -0,0 +1,13 @@
+package com.example.ThaiAnhNhat2216;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaiAnhNhat2216Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaiAnhNhat2216Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThaiAnhNhat2216/ThaiAnhNhat2216ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThaiAnhNhat2216;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThaiAnhNhat2216ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
